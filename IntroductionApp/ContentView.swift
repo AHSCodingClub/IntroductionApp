42e94d5f0231d63a565d3bf1cf956db415e3e6db
Zheng <aheze@getfind.app>
2022-01-18 17:25:09 -0800

    Initial Commit

---
+++ IntroductionApp/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  IntroductionApp
+//
+//  Created by A. Zheng (github.com/aheze) on 1/18/22.
+//  Copyright © 2022 A. Zheng. All rights reserved.
+//
+    
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IntroductionApp/IntroductionAppApp.swift
@@ -0,0 +1,19 @@
+//
+//  IntroductionAppApp.swift
+//  IntroductionApp
+//
+//  Created by A. Zheng (github.com/aheze) on 1/18/22.
+//  Copyright © 2022 A. Zheng. All rights reserved.
+//
+    
+
+import SwiftUI
+
+@main
+struct IntroductionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
