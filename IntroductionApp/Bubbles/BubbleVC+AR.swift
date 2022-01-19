b90466e12c41c7789cad3a19c1d0df53bca6285d
Zheng <aheze@getfind.app>
2022-01-19 10:42:52 -0800

    Add more bubbles

--- IntroductionApp/Bubbles/BubbleVC+AR.swift
+++ IntroductionApp/Bubbles/BubbleVC+AR.swift
@@ -33,31 +33,56 @@ extension BubbleViewController {
         model.spawn = { [weak self] in
             guard let self = self else { return }
             if let position = self.getPosition() {
-                let sphereGeometry = SCNSphere(radius: 0.12)
-                
-                let material = SCNMaterial()
-                material.lightingModel = .blinn
-                material.transparencyMode = .dualLayer
-                material.fresnelExponent = 1.5
-                material.isDoubleSided = true
-                material.specular.contents = UIColor(white: 0.6, alpha: 1.0)
-                material.diffuse.contents = UIImage(named: "Texture")!
-                material.shininess =  80
-                material.reflective.contents = UIColor.gray.withAlphaComponent(0.7)
-                sphereGeometry.materials = [material]
-                
-                let node = SCNNode(geometry: sphereGeometry)
-                node.position = SCNVector3(
+                let basePosition = SCNVector3(
                     x: position.x,
-                    y: position.y,
+                    y: position.y + 1.2,
                     z: position.z
                 )
-                self.nodes.append(node)
-                self.sceneView.scene.rootNode.addChildNode(node)
+                
+                for row in -5..<5 {
+                    for column in -5..<5 {
+//                        let position =
+                        let rowOffset = Float(row) * 0.3
+                        let columnOffset = Float(column) * 0.3
+                        let position = SCNVector3(
+                            x: position.x + rowOffset,
+                            y: position.y,
+                            z: position.z + columnOffset
+                        )
+                        self.spawnSphere(position: position)
+                    }
+                }
             }
         }
     }
     
+    func spawnSphere(position: SCNVector3) {
+        let sphereGeometry = SCNSphere(radius: CGFloat.random(in: 0.11...0.14))
+        
+        let material = SCNMaterial()
+        material.lightingModel = .blinn
+        material.transparencyMode = .dualLayer
+        material.fresnelExponent = 1.5
+        material.isDoubleSided = true
+        material.specular.contents = UIColor(white: 0.6, alpha: 1.0)
+        material.diffuse.contents = UIImage(named: "Texture")!
+        material.shininess =  80
+        material.reflective.contents = UIColor.gray.withAlphaComponent(0.7)
+        sphereGeometry.materials = [material]
+        
+        let node = SCNNode(geometry: sphereGeometry)
+        node.position = position
+        let moveDown = SCNAction.move(by: SCNVector3(0, -CGFloat.random(in: 0.08...0.1), 0), duration: CGFloat.random(in: 0.5...2))
+        let moveUp = SCNAction.move(by: SCNVector3(0, CGFloat.random(in: 0.08...0.1), 0), duration: CGFloat.random(in: 0.5...2))
+        let waitAction = SCNAction.wait(duration: 0.25)
+        let hoverSequence = SCNAction.sequence([moveUp, waitAction, moveDown])
+        let loopSequence = SCNAction.repeatForever(hoverSequence)
+        node.runAction(loopSequence)
+        
+        
+        self.nodes.append(node)
+        self.sceneView.scene.rootNode.addChildNode(node)
+    }
 
     func getPosition() -> simd_float4? {
         let touchLocation = CGPoint(x: view.frame.midX, y: view.frame.midY)
